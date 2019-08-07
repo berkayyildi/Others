7472d3af2b4cf092520026c5ca1bc1010ffb77a0
Berkay YILDIZ <20827063+berkayyildi@users.noreply.github.com>
2019-08-07 11:54:21 +0300

    Add files via upload

---
+++ redir.php
@@ -0,0 +1,23 @@
+
+<a href='?redirect=http://localhost/?secret_token=PASSWORD'>Allowed</a>
+<a href='?redirect=http://google.com/?secret_token=PASSWORD'>Disallowed</a>
+<a href='?redirect=https://google.com?q=PWNED (Check The Referer TAG)&redirect=http://localhost/?secret_token=PASSWORD'>JS HPP</a>
+
+<?php
+if ($_GET['redirect']){
+    $redirect_domain = parse_url($_GET['redirect'], PHP_URL_HOST);
+    if ($redirect_domain != $_SERVER['SERVER_NAME']){
+        die("<br>Just same domain url redirect allowed!");
+    }
+}
+?>
+
+
+<script>
+    var url = new URL(document.URL);
+    var redirurl = url.searchParams.get("redirect");
+    if (redirurl !== null){
+        document.write("You are redirecting...");
+        window.location = redirurl;
+    }
+</script>
